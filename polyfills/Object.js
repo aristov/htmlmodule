04e1cc6d1e46385daa876e5a1014ae9fe1b58e6e
aristov <vv.aristov@gmail.com>
2016-07-27 18:06:55 +0300

    clear code

--- polyfills/Object.js
+++ polyfills/Object.js
@@ -1,8 +1,5 @@
-//export const values = Object => {
-    //console.log('fuck');
-    if(!Object.values) Object.values = function(object) {
-        return Object.keys(object).map(function(key) {
-            return object[key];
-        });
-    };
-//};
+if(!Object.values) Object.values = function(object) {
+    return Object.keys(object).map(function(key) {
+        return object[key];
+    });
+};
