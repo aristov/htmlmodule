b051e94a3fae023ceeaabb780d88bf242fb81349
aristov <vv.aristov@gmail.com>
2016-10-05 00:44:14 +0300

    saucelabs launcher

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,15 @@
 // Karma configuration
 // Generated on Mon Oct 03 2016 16:34:46 GMT+0300 (MSK)
 
+const customLaunchers = {
+    SL_Chrome: {
+        base: 'SauceLabs',
+        platform: 'OS X 10.11',
+        browserName: 'chrome',
+        version: '53.0'
+    }
+}
+
 module.exports = function(config) {
   config.set({
 
@@ -52,7 +61,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: ['progress', 'mocha', 'saucelabs'],
 
 
     // web server port
@@ -71,11 +80,24 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
+    sauceLabs: {
+        testName: 'Instance.js unit tests',
+        recordScreenshots: false,
+        connectOptions: {
+            port: 5757,
+            logfile: 'sauce_connect.log'
+        },
+        public: 'public'
+    },
+
+    captureTimeout: 120000,
+
+    customLaunchers,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
+    // browsers: ['Sauce'],
+    browsers: Object.keys(customLaunchers),
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
