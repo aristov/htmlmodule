e28a7f7b9f4cd991b75e41791bed17357820893e
aristov <vv.aristov@gmail.com>
2016-10-07 14:04:09 +0300

    serializer

---
+++ html/html.serializer.js
@@ -0,0 +1,39 @@
+const map = Array.prototype.map;
+const assign = Object.assign;
+
+const noEndTagList = 'area base br embed hr img input keygen link meta param source track wbr';
+const noEndTagSet = noEndTagList.split(' ').reduce((res, tag) => (res[tag] = true, res), {});
+
+export class HTMLSerializer {
+    constructor(options = {
+        indent : '    ',
+        lineBreak : '\n',
+        level : 0
+    }) {
+        assign(this, options);
+    }
+    serializeToString({ tagName, attributes, childNodes, textContent }) {
+        const lineBreak = this.lineBreak;
+        const indent = this.indent.repeat(this.level);
+        let result = indent;
+        if(tagName) {
+            tagName = tagName.toLowerCase();
+            result += '<' + tagName;
+            if(attributes && attributes.length) {
+                const attrset = map.call(attributes, ({ name, value }) => ` ${name}="${value}"`);
+                result += attrset.join('');
+            }
+            result += '>';
+            if(childNodes && childNodes.length) {
+                this.level++;
+                const children = map.call(childNodes, this.serializeToString, this);
+                result += lineBreak + children.join('');
+            }
+            if(!noEndTagSet[tagName]) result += indent + `</${tagName}>`;
+        } else {
+            result += textContent;
+        }
+        result += lineBreak;
+        return result;
+    }
+}
--- repl/repl.js
+++ repl/repl.js
@@ -1,48 +1,28 @@
 import '../shim/shim';
 
-import { main, div, textarea, pre, code } from '../htmldom/htmldom';
+import { output, main, div, textarea, pre, code } from '../htmldom/htmldom';
 import * as HTMLDOM from '../htmldom/htmldom';
 
-import victorica from 'victorica';
 import hljs from 'highlight.js/';
 import 'highlight.js/styles/agate.css';
 
-const victoricaConfig = { space: '    ' };
-
-const src = require('raw!./repl.value.rawjs');
+import value from 'raw!./repl.value.rawjs';
+import { HTMLSerializer } from '../html/html.serializer';
 
 const vars = Object.keys(HTMLDOM).map(name => name + '=hd.' + name).join(',');
 
-const fnbody = value => `var ${vars};out.append(${value});`;
+const fnbody = value => `var ${vars};${value};out.append(dom);`;
 
-function evaluate(value) {
-    value = String(value).trim();
-    app.classList.remove('invalid');
-    if(value) {
-        try {
-            const fn = new Function('hd', 'out', fnbody(value));
-            domoutput.textContent = '';
-            fn(HTMLDOM, domoutput);
-            codepre.firstChild.replaceWith(code(victorica(domoutput.innerHTML, victoricaConfig)));
-            hljs.highlightBlock(codepre);
-        } catch(error) {
-            domoutput.textContent = error;
-            codepre.textContent = 'Error!';
-            app.classList.add('invalid');
-        }
-    } else domoutput.textContent = '';
-}
+const panel = children => div({ className : 'panel', children });
 
 const codeinput = textarea({
     className : 'codeinput',
     placeholder : 'Type HTMLDOM code here...',
-    textContent : src,
+    textContent : value,
     oninput : ({ target : { value } }) => evaluate(value)
 });
 
-const domoutput = div({ className : 'domoutput' });
-
-const panel = children => div({ className : 'panel', children });
+const domoutput = output({ className : 'domoutput' });
 
 const codepre = pre({ className : 'codeoutput html', children : code() });
 
@@ -53,8 +33,30 @@ const app = main({
         panel(domoutput),
         panel(codepre)
     ]
-})
+});
 
-document.body.append(app);
+const serializer = new HTMLSerializer;
+
+function evaluate(value) {
+    value = String(value).trim();
+    app.classList.remove('invalid');
+    if(value) {
+        try {
+            const fn = new Function('hd', 'out', fnbody(value));
+            domoutput.textContent = '';
+            fn(HTMLDOM, domoutput);
+
+            const htmlcode = code(serializer.serializeToString(domoutput.firstChild));
+            codepre.firstChild.replaceWith(htmlcode);
+            hljs.highlightBlock(codepre);
+        } catch(error) {
+            domoutput.textContent = error;
+            codepre.textContent = 'Error!';
+            app.classList.add('invalid');
+        }
+    } else domoutput.textContent = '';
+}
 
 evaluate(codeinput.value);
+
+document.body.append(app);
