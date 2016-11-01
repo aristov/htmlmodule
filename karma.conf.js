0202406e082f5b481e5122097e4f1c57205c952f
aristov <vv.aristov@gmail.com>
2016-11-01 20:06:03 +0300

    try to setup istanbul

--- karma.conf.js
+++ karma.conf.js
@@ -1,76 +1,94 @@
+const path = require('path');
+
 module.exports = function(config) {
     const customLaunchers = {
-        SL_Chrome: {
-            base: 'SauceLabs',
-            platform: 'OS X 10.11',
-            browserName: 'chrome',
-            version: '53.0'
+        SL_Chrome : {
+            base : 'SauceLabs',
+            platform : 'OS X 10.11',
+            browserName : 'chrome',
+            version : '53.0'
         },
-        /*SL_Firefox: {
-            base: 'SauceLabs',
-            platform: 'Linux',
-            browserName: 'firefox',
-            version: '45.0'
+        /*SL_Firefox : {
+            base : 'SauceLabs',
+            platform : 'Linux',
+            browserName : 'firefox',
+            version : '45.0'
         },*/
-        /*SL_Safari: {
-            base: 'SauceLabs',
-            platform: 'OS X 10.11',
-            browserName: 'safari',
-            version: '9.0'
+        /*SL_Safari : {
+            base : 'SauceLabs',
+            platform : 'OS X 10.11',
+            browserName : 'safari',
+            version : '9.0'
         },*/
-        /*SL_MSIE11: {
-            base: 'SauceLabs',
-            platform: 'Windows 8.1',
-            browserName: 'internet explorer',
-            version: '11'
+        /*SL_MSIE11 : {
+            base : 'SauceLabs',
+            platform : 'Windows 8.1',
+            browserName : 'internet explorer',
+            version : '11'
         },*/
-        /*SL_MSIE9: {
-            base: 'SauceLabs',
-            platform: 'Windows 7',
-            browserName: 'internet explorer',
-            version: '9'
+        /*SL_MSIE9 : {
+            base : 'SauceLabs',
+            platform : 'Windows 7',
+            browserName : 'internet explorer',
+            version : '9'
         }*/
     }
     config.set({
-        basePath: '',
-        frameworks: ['mocha', 'sinon'],
-        files: ['dist/dist.shim.min.js', 'lib/index.spec.js'],
-        exclude: [],
-        preprocessors: {
-            'lib/index.spec.js': ['webpack'],
-            'lib/htmlassembler.js': ['coverage'],
-        },
-        webpack: {
-            module: {
-                loaders: [
-                    { test: /\.js$/, loader: 'babel?presets[]=es2015' }
+        basePath : '',
+        frameworks : ['mocha', 'sinon'],
+        files : [
+            'dist/dist.shim.js',
+            // 'lib/index.spec.js',
+            'dist/dist.spec.js',
+        ],
+        exclude : [],
+        /*preprocessors : {
+            'lib/index.spec.js' : [
+                'webpack',
+                // 'coverage'
+            ],
+        },*/
+        /*webpack : {
+            module : {
+                preLoaders : [{
+                    test : /[^(\.spec)]\.js$/,
+                    loader : 'babel-istanbul!babel?presets[]=es2015',
+                    exclude : [
+                        path.resolve('node_modules/')
+                    ]
+                }],
+                loaders : [
+                    {
+                        test : /\.spec\.js$/,
+                        loader : 'babel?presets[]=es2015'
+                    }
                 ]
-            }
-        },
-        webpackMiddleware: {
-            noInfo: true,
-            stats: { chunks: false }
-        },
-        reporters: ['mocha', 'coverage'],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        sauceLabs: {
-            username: 'aristov7',
-            accessKey: '0e922c40-5658-44fc-ab2b-7be69c51301e',
-            testName: 'htmlmodule spec',
-            recordScreenshots: false,
-            connectOptions: {
-                port: 5757,
-                logfile: 'sauce_connect.log'
             },
-            public: 'public'
+        },*/
+        /*webpackMiddleware : {
+            noInfo : true,
+            stats : { chunks : false }
+        },*/
+        reporters : ['mocha', 'coverage'],
+        port : 9876,
+        colors : true,
+        logLevel : config.LOG_INFO,
+        autoWatch : true,
+        sauceLabs : {
+            username : 'aristov7',
+            accessKey : '0e922c40-5658-44fc-ab2b-7be69c51301e',
+            testName : 'htmlmodule',
+            recordScreenshots : false,
+            connectOptions : {
+                port : 5757,
+                logfile : 'sauce_connect.log'
+            },
+            public : 'public'
         },
-        captureTimeout: 120000,
+        captureTimeout : 120000,
         customLaunchers,
-        browsers: Object.keys(customLaunchers),
-        singleRun: true,
-        concurrency: Infinity
+        browsers : Object.keys(customLaunchers),
+        singleRun : true,
+        concurrency : Infinity
     })
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,24 @@ const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 
 const env = process.env;
+const preLoaders = [];
 const plugins = [];
+const suffix = env.MIN? '.min.js' : '.js';
+
+const babelLoader = env.ES6? {
+    test : /\.js$/,
+    loader : 'babel?plugins[]=transform-es2015-modules-commonjs'
+} : {
+    test : /\.js$/,
+    loader : 'babel?presets[]=es2015'
+};
+
+const rawLoader = { test : /\.txt$/, loader : 'raw-loader' };
+
+const styleLoader = {
+    test : /\.css$/,
+    loader : 'style-loader!css-loader!postcss-loader'
+};
 
 if(env.MIN) {
     if(env.ES6) throw Error('Minification requires transpiling to ES5');
@@ -16,32 +33,74 @@ if(env.MIN) {
     plugins.push(uglifyjsPlugin);
 }
 
+if(env.COV) {
+    preLoaders.push({
+        test : /[^(\.spec)]\.js$/,
+        loader : 'babel-istanbul!babel?presets[]=es2015',
+        exclude: [
+            path.resolve('node_modules/')
+        ],
+    });
+}
+
 module.exports = {
-    entry : env.DOCS? {
-        shim : ['./shim'],
-        index : ['./docs'],
-        spec : ['./lib/index.spec']
-    } : {
-        shim : ['./shim'],
-        htmlmodule : ['./lib'],
-        'window.htmlmodule' : ['./lib/window.htmlmodule']
-    },
-    output : {
-        path : path.join(__dirname, '/dist'),
-        filename : env.MIN? 'dist.[name].min.js' : 'dist.[name].js',
-        pathinfo : !env.MIN
-    },
-    module : {
-        loaders : [
-            env.ES6?
-                { test : /\.js$/, loader : 'babel?plugins[]=transform-es2015-modules-commonjs' } :
-                { test : /\.js$/, loader : 'babel?presets[]=es2015' },
-            { test : /\.txt$/, loader : 'raw-loader' },
-            { test : /\.css$/, loader : 'style-loader!css-loader!postcss-loader' },
-        ]
-    },
     plugins,
     resolve : { modulesDirectories : ['node_modules'] },
-    postcss : () => [autoprefixer],
     watch : Boolean(env.WATCH)
 };
+
+const assign = Object.assign;
+
+switch(env.ENTRY) {
+    case 'spec':
+        assign(module.exports, {
+            entry : {
+                spec : ['./lib/index.spec']
+            },
+            output : {
+                path : path.join(__dirname, '/dist'),
+                filename : 'dist.[name]' + suffix,
+                pathinfo : !env.MIN
+            },
+            module : {
+                preLoaders,
+                loaders : [babelLoader],
+            }
+        });
+        break;
+    case 'index':
+        assign(module.exports, {
+            entry : {
+                index : ['./docs'],
+            },
+            output : {
+                path : path.join(__dirname, '/dist'),
+                filename : 'dist.[name]' + suffix,
+                pathinfo : !env.MIN
+            },
+            module : {
+                loaders : [babelLoader, rawLoader, styleLoader],
+            },
+            postcss : () => [autoprefixer],
+        });
+        break;
+    case 'dist':
+    default:
+        assign(module.exports, {
+            entry : {
+                shim : ['./shim'],
+                htmlmodule : ['./lib'],
+                'window.htmlmodule' : ['./lib/window.htmlmodule']
+            },
+            output : {
+                path : path.join(__dirname, '/dist'),
+                filename : 'dist.[name]' + suffix,
+                pathinfo : !env.MIN
+            },
+            module : {
+                preLoaders,
+                loaders : [babelLoader],
+            }
+        });
+        break;
+}
