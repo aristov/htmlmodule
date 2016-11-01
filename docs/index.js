42cf073e06df07dcc21f4e4983a02564d692f44a
aristov <vv.aristov@gmail.com>
2016-11-01 13:11:15 +0300

    precommit

--- docs/index.js
+++ docs/index.js
@@ -2,7 +2,6 @@ import { REPLApp } from './lib/replapp';
 import { data } from './data';
 import './index.css';
 
-// use module C without knowledge about async nature of A and B
 const replapp = new REPLApp(data);
 
 document.body.append(replapp.node);
