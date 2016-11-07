fd87bb3026d49c12f924e0d7e8b2242a25befcc6
aristov <vv.aristov@gmail.com>
2016-11-08 01:58:44 +0300

    remove DOM Element related shims

--- shim/index.js
+++ shim/index.js
@@ -1,8 +1,8 @@
 import 'babel-polyfill';
 import 'dom4';
 import 'whatwg-fetch';
-import './shim.id';
-import './shim.classname';
+// import './shim.id';
+// import './shim.classname';
 import './shim.classlist';
 import './shim.hidden';
 import './shim.click';
