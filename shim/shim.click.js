b55c12c84e8b34bbccaac94923b9ac30a52028b3
aristov <vv.aristov@gmail.com>
2016-11-14 20:14:55 +0300

    fix focus

--- shim/shim.click.js
+++ shim/shim.click.js
@@ -5,21 +5,44 @@
  *
  * @polyfill
  */
-const span = document.createElement('span');
-if('click' in span && 'onclick' in span) {
-    let called = null;
-    span.onclick = event => called = event;
-    span.click();
-    if(!called) {
-        const proto = HTMLElement.prototype;
-        const method = proto.click;
-        proto.click = function() {
-            method.call(this);
-            if('onclick' in this && typeof this.onclick === 'function') {
-                const event = document.createEvent('Event');
-                event.initEvent('click', true, true);
-                this.onclick(event);
-            }
-        };
+{
+    const span = document.createElement('span');
+    if('click' in span && 'onclick' in span) {
+        let called = null;
+        span.onclick = event => called = event;
+        span.click();
+        if(!called) {
+            const proto = HTMLElement.prototype;
+            const method = proto.click;
+            proto.click = function() {
+                method.call(this);
+                if('onclick' in this && typeof this.onclick === 'function') {
+                    const event = document.createEvent('Event');
+                    event.initEvent('click', true, true);
+                    this.onclick(event);
+                }
+            };
+        }
+    }
+}
+
+{
+    const span = document.createElement('span');
+    if('focus' in span && 'onfocus' in span) {
+        let called = null;
+        span.onfocus = event => called = event;
+        span.focus();
+        if(!called) {
+            const proto = HTMLElement.prototype;
+            const method = proto.focus;
+            proto.focus = function() {
+                method.call(this);
+                if('onfocus' in this && typeof this.onfocus === 'function') {
+                    const event = document.createEvent('Event');
+                    event.initEvent('focus', true, true);
+                    this.onfocus(event);
+                }
+            };
+        }
     }
 }
