38eac137c43231b99c169447cb65ae4872f17722
aristov <vv.aristov@gmail.com>
2016-11-15 03:01:40 +0300

    refactor a bit more

--- shim/shim.click.js
+++ shim/shim.click.js
@@ -4,24 +4,23 @@
  *
  * @polyfill
  */
-{
-    const span = document.createElement('span');
-    if('click' in span && 'onclick' in span) {
-        let called = null;
-        span.onclick = event => called = event;
-        span.click();
-        if(!called) {
-            const proto = HTMLElement.prototype;
-            const method = proto.click;
-            proto.click = function() {
-                method.call(this);
-                if('onclick' in this && typeof this.onclick === 'function') {
-                    this.onclick(new MouseEvent('click', {
-                        bubbles : true,
-                        cancelable : true
-                    }));
-                }
-            };
-        }
+const { MouseEvent } = window;
+const span = document.createElement('span');
+if('click' in span && 'onclick' in span) {
+    let called = null;
+    span.onclick = event => called = event;
+    span.click();
+    if(!called) {
+        const proto = HTMLElement.prototype;
+        const method = proto.click;
+        proto.click = function() {
+            method.call(this);
+            if('onclick' in this && typeof this.onclick === 'function') {
+                this.onclick(new MouseEvent('click', {
+                    bubbles : true,
+                    cancelable : true
+                }));
+            }
+        };
     }
 }
