111526d71e641b9c469c9aa4e4865f16e391f26c
aristov <vv.aristov@gmail.com>
2016-11-19 20:25:27 +0300

    testcase event handlers description

--- docs/data/index.js
+++ docs/data/index.js
@@ -7,7 +7,6 @@ import apinav from 'raw!./apinav.rawjs';
 import preformatted from 'raw!./preformatted.rawjs';
 import richform from 'raw!./richform.rawjs';
 import specwin from 'raw!./specwin.rawjs';
-import scriptstyle from 'raw!./scriptstyle.rawjs';
 import tabulardata from 'raw!./tabulardata.rawjs';
 import testcase from 'raw!././testcase.rawjs';
 import websearch from 'raw!./websearch.rawjs';
@@ -15,15 +14,14 @@ import websearch from 'raw!./websearch.rawjs';
 export const data = [
     intro,
     apinav,
-    websearch,
     eventhandlers,
+    websearch,
     headgroup,
     richform,
     testcase,
     authfieldset,
     tabulardata,
     preformatted,
-    // scriptstyle,
     fetchsource,
     specwin,
 ];
