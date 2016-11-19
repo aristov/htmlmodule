57b7f4ee321b2382c1691547bca05b7df3902b11
aristov <vv.aristov@gmail.com>
2016-11-19 15:24:30 +0300

    codemirror styling and jsdoc

--- docs/lib/codemirror.js
+++ docs/lib/codemirror.js
@@ -7,9 +7,6 @@ import 'codemirror/mode/htmlmixed/htmlmixed';
 import 'codemirror/lib/codemirror.css';
 import './codemirror.css';
 
-import 'codemirror/theme/rubyblue.css';
-const theme = 'rubyblue';
-
 const CODE_BOX_DEFAULTS = {
     mode : 'javascript',
     indentUnit : 4,
@@ -20,44 +17,87 @@ const CODE_BOX_DEFAULTS = {
     autoCloseBrackets : true,
     matchBrackets : true,
     smartIndent : true,
-    theme
 };
 
 const MARKUP_BOX_DEFAULTS = {
     mode : 'htmlmixed',
     readOnly : true,
-    theme
 };
 
 export class CodeMirrorAssembler extends HTMLDOMAssembler {
+    /**
+     * Assembles codemirror editor instance
+     * @param {Object} init
+     */
     constructor(init) {
         super(init);
         this.assemble('div', NodeInit(init));
     }
+
+    /**
+     * Set the editor value
+     * @param {String} value
+     */
     set value(value) {
         this.mirror.setValue(value && value.toString());
     }
+
+    /**
+     * Get the editor value
+     * @returns {String}
+     */
     get value() {
         return this.mirror.getValue();
     }
+
+    /**
+     * Get the editor client height
+     * @returns {Number}
+     */
     get height() {
         return this.element.clientHeight;
     }
+
+    /**
+     * Set the onchange event handler
+     * @param onchange
+     */
     set onchange(onchange) {
         this.mirror.on('change', onchange);
     }
+
+    /**
+     * Set a unique id on the editor's textarea element
+     * @param id
+     */
     set id(id) {
         this.element.querySelector('textarea').id = id;
     }
+
+    /**
+     * Assemble widget DOM structure
+     * @param tagName
+     * @param init
+     */
     assemble(tagName, init) {
         super.assemble(tagName);
         this.createMirror(init.options);
         delete init.options;
         this.init(init);
     }
+
+    /**
+     * Create the CodeMirror instance
+     * @param options
+     * @returns {*}
+     */
     createMirror(options) {
         return this.mirror = new CodeMirror(this.element, options);
     }
+
+    /**
+     * Refresh the CodeMirror instance
+     */
     refresh() {
         this.mirror.refresh();
     }
