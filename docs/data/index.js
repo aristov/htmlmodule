977d35c8ffad48c1ced74121bbcd35d7c1438fb2
aristov <vv.aristov@gmail.com>
2016-11-20 05:41:08 +0300

    testcase data wip

--- docs/data/index.js
+++ docs/data/index.js
@@ -1,28 +1,24 @@
-import authfieldset from 'raw!./authfieldset.rawjs';
-import eventhandlers from 'raw!./eventhandlers.rawjs';
-import fetchsource from 'raw!./fetchsource.rawjs';
-import headgroup from 'raw!./headgroup.rawjs';
 import intro from 'raw!./intro.rawjs';
 import apinav from 'raw!./apinav.rawjs';
-import preformatted from 'raw!./preformatted.rawjs';
-import richform from 'raw!./richform.rawjs';
-import specwin from 'raw!./specwin.rawjs';
-import tabulardata from 'raw!./tabulardata.rawjs';
-import testcase from 'raw!././testcase.rawjs';
+import sections from 'raw!./sections.rawjs';
+import grouping from 'raw!./grouping.rawjs';
 // import textlevel from 'raw!././textlevel.rawjs';
+import forms from 'raw!./forms.rawjs';
+import eventhandlers from 'raw!./eventhandlers.rawjs';
+import tabulardata from 'raw!./tabulardata.rawjs';
+// import fetchsource from 'raw!./fetchsource.rawjs';
+import specwin from 'raw!./specwin.rawjs';
 import websearch from 'raw!./websearch.rawjs';
 
 export const data = [
     intro,
     apinav,
+    sections,
+    grouping,
+    // textlevel,
+    forms,
     eventhandlers,
     tabulardata,
-    websearch,
-    headgroup,
-    richform,
-    testcase,
-    authfieldset,
-    preformatted,
-    fetchsource,
     specwin,
+    websearch,
 ];
