e6d6e8d5bbb29548cf0b0af6cb3e27df9abfeecb
aristov <vv.aristov@gmail.com>
2016-11-20 16:44:59 +0300

    test case: section, grouping and text

--- docs/data/index.js
+++ docs/data/index.js
@@ -2,11 +2,10 @@ import intro from 'raw!./intro.rawjs';
 import apinav from 'raw!./apinav.rawjs';
 import sections from 'raw!./sections.rawjs';
 import grouping from 'raw!./grouping.rawjs';
-// import textlevel from 'raw!././textlevel.rawjs';
+import textlevel from 'raw!././textlevel.rawjs';
 import forms from 'raw!./forms.rawjs';
 import eventhandlers from 'raw!./eventhandlers.rawjs';
 import tabulardata from 'raw!./tabulardata.rawjs';
-// import fetchsource from 'raw!./fetchsource.rawjs';
 import specwin from 'raw!./specwin.rawjs';
 import websearch from 'raw!./websearch.rawjs';
 
@@ -15,7 +14,7 @@ export const data = [
     apinav,
     sections,
     grouping,
-    // textlevel,
+    textlevel,
     forms,
     eventhandlers,
     tabulardata,
