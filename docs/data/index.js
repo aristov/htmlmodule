1bd65416a4b5255b84565c82a1ccca91eec824c9
aristov <vv.aristov@gmail.com>
2016-11-25 02:52:07 +0300

    embedded content

--- docs/data/index.js
+++ docs/data/index.js
@@ -3,10 +3,10 @@ import apinav from 'raw!./apinav.rawjs';
 import sections from 'raw!./sections.rawjs';
 import grouping from 'raw!./grouping.rawjs';
 import textlevel from 'raw!././textlevel.rawjs';
+import embedded from 'raw!./embedded.rawjs';
+import tabulardata from 'raw!./tabulardata.rawjs';
 import forms from 'raw!./forms.rawjs';
 import eventhandlers from 'raw!./eventhandlers.rawjs';
-import tabulardata from 'raw!./tabulardata.rawjs';
-// import specwin from 'raw!./specwin.rawjs';
 // import websearch from 'raw!./websearch.rawjs';
 
 export const data = [
@@ -16,8 +16,8 @@ export const data = [
     grouping,
     textlevel,
     forms,
+    embedded,
     tabulardata,
     eventhandlers,
-    // specwin,
     // websearch,
 ];
