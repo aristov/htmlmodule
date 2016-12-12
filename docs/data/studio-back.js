0aee92d04cef98195692a13b8f2520ef1995e7de
aristov <vv.aristov@gmail.com>
2016-12-12 20:37:22 +0300

    studio (wip)

---
+++ docs/data/studio-back.js
@@ -0,0 +1,25 @@
+if(!document.getElementById('studio-back')) {
+    const { style } = htmlmodule;
+    const urlset = [
+        'https://pp.vk.me/c630431/v630431762/4d6ba/H8uH_TMSVrA.jpg',
+        'https://pp.vk.me/c630119/v630119762/4f88b/HuiiHuD7hdY.jpg',
+        'https://pp.vk.me/c630119/v630119762/4f895/KnbZMuCAvPA.jpg',
+        'https://pp.vk.me/c630431/v630431762/4d6b1/CiRGYeiCs7E.jpg',
+        'https://pp.vk.me/c631528/v631528762/35696/DWnfZGh4kgw.jpg',
+        'https://pp.vk.me/c631528/v631528762/35689/2OGae1WAvsU.jpg',
+        'https://pp.vk.me/c631528/v631528762/3565d/gUEfXtr0XAs.jpg',
+        'https://pp.vk.me/c631528/v631528762/35654/VAuxe11lD24.jpg',
+        'https://pp.vk.me/c631528/v631528762/3564b/1CX4aova9R4.jpg',
+        'https://pp.vk.me/c604529/v604529762/ac95/UoVGNFhm7Do.jpg',
+        'https://pp.vk.me/c604529/v604529762/ac83/rKFx4LAiT3s.jpg',
+        'https://pp.vk.me/c626330/v626330762/bac9/5DjpZYB2Uw4.jpg',
+        'https://pp.vk.me/c630626/v630626566/1aed8/lKmjB_4QWmo.jpg',
+        'https://pp.vk.me/c626128/v626128762/3b35d/-pha-dvzI30.jpg',
+    ]
+    const random = Math.floor(Math.random() * 100 % urlset.length)
+    const url = urlset[random]
+    document.head.append(style({
+        id : 'studio-back',
+        children : `body::before { background-image: url(${ url }) }`
+    }))
+}
--- docs/data/studio.js
+++ docs/data/studio.js
@@ -1,95 +1,126 @@
 const { htmldom, a, abbr, address, area, article, aside, audio, b, base, bdi, bdo, blockquote, body, br, button, canvas, caption, cite, code, col, colgroup, datalist, dd, del, details, dfn, div, dl, dt, em, embed, fieldset, figcaption, figure, footer, form, h1, h2, h3, h4, h5, h6, head, header, hgroup, hr, html, i, iframe, img, input, ins, kbd, label, legend, li, link, main, map, mark, meta, nav, noscript, object, ol, optgroup, option, p, param, picture, pre, progress, q, rp, rt, ruby, s, samp, script, section, select, small, source, span, strong, style, sub, summary, sup, table, tbody, td, textarea, tfoot, th, thead, title, tr, track, u, ul, variable, video, wbr } = htmlmodule
 
-let layout = pre()
+const layout = pre()
 
 fetch('/htmlmodule/docs/data/studio.txt')
     .then(res => res.text())
     .then(res => layout.textContent = res)
 
-document.body = body([
-    header([
-        h1({
-            title : 'Музыкальная студия',
-            children : 'Фьюжн'
-        }),
-        address({
-            title : 'Контактные телефоны',
-            className : 'phonecontact dim',
-            children : ul([
-                li('+7 926 602 2587'),
-                li('+7 916 56 808 56')
-            ])
-        }),
-    ]),
-    main([
-        address([
-            p(a({
-                href : 'https://yandex.ru/maps/-/CZHMuBJ8',
-                title : 'Адрес студии',
-                target : '_blank',
-                children : 'Москва, 2-я Звенигородская улица, дом 13, строение 17'
-            })),
-            details({
-                open : true,
-                style : { padding : '10px 0 20px' },
-                children : [
-                    summary({
-                        className : 'dim',
-                        innerHTML : 'Метро &laquo;Улица 1905 года&raquo;'
-                    }),
-                    div({
-                        style : { padding : '10px 0 0' },
-                        children : a({
-                            href : 'https://yandex.ru/maps/?um=mymaps%3A4L2HMAQC2mYDutWIKsc2LAD98hZTzzSe&amp;source=constructorStatic',
-                            target : '_blank',
-                            children : img({
-                                src : 'https://api-maps.yandex.ru/services/constructor/1.0/static/?sid=4L2HMAQC2mYDutWIKsc2LAD98hZTzzSe&amp;width=600&amp;height=450&amp;lang=ru_RU&amp;sourceType=mymaps',
-                                alt : 'Путь от метро до студии',
-                                style : { border : '0' }
-                            })
+const CURRENT_YEAR = (new Date).getFullYear()
 
+const time = init => htmldom('time', init)
+const nbsp = (count = 1) => ' '.repeat(count)
+const hash = location.hash.replace(/^#/, '')
+
+document.body = body({
+    children : [
+        div([
+            header({
+                className : 'siteheader',
+                children : [
+                    h1({
+                        title : 'Музыкальная студия',
+                        className : 'studiologo',
+                        children : a({
+                            // href : 'https://fusion1905.ru',
+                            className : 'dim',
+                            children : 'Fusion'
                         })
-                    })
+                    }),
+                    address({
+                        title : 'Контактные телефоны',
+                        className : 'dim',
+                        children : ul([
+                            li('+7 926 602 2587'),
+                            li('+7 916 56 808 56')
+                        ])
+                    }),
                 ]
             }),
-        ])
-    ]),
-    0 && section([
-        details({
-            open : true,
-            style : '',
-            children : [
-                summary('Пространство студии'),
-                div({
-                    className : 'layoutart',
-                    children : figure(layout)
-                }),
-            ]
-        })
-    ]),
-    footer({
-        className : 'contentinfo',
-        children : [
-            small({
-                title : 'Время работы студии',
-                className : 'dim',
-                innerHTML : [
-                    '&#128338;',
-                    '&nbsp;&nbsp;&nbsp;',
-                    '<time>07:00</time>',
-                    '&nbsp;&mdash;&nbsp;',
-                    '<time>02:00</time>'
-                ].join('')
+            main({
+                className : 'sitecontent',
+                children : [
+                    address([
+                        p(a({
+                            href : 'https://yandex.ru/maps/-/CZHMuBJ8',
+                            rel : 'external',
+                            title : 'Адрес студии',
+                            target : '_blank',
+                            className : 'dim',
+                            children : 'Москва, 2-я Звенигородская улица, дом 13, строение 17'
+                        })),
+                        details({
+                            open : hash === 'pathmap',
+                            className : 'pathmap',
+                            children : [
+                                summary({
+                                    className : 'dim',
+                                    innerHTML : 'Пеший маршрут от метро &laquo;Улица 1905 года&raquo;'
+                                }),
+                                div(script({
+                                    type : 'text/javascript',
+                                    charset : 'utf-8',
+                                    async : true,
+                                    src : 'https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=4L2HMAQC2mYDutWIKsc2LAD98hZTzzSe&amp;width=1000&amp;height=720&amp;lang=ru_RU&amp;sourceType=mymaps&amp;scroll=true'
+                                }))
+                            ]
+                        }),
+                    ]),
+                    section(details({
+                        open : hash === 'spacemap',
+                        children : [
+                            summary({
+                                className : 'dim',
+                                children : 'Пространство студии'
+                            }),
+                            div({
+                                className : 'spacemap',
+                                children : figure(layout)
+                            }),
+                        ]
+                    })),
+                ]
             }),
-            address(small(a({
-                href : 'mailto:vv.aristov@gmail.com',
-                className : 'dim',
-                innerHTML : '&copy; pypik'
-            })))
-        ]
-    }),
+        ]),
+        footer({
+            className : 'contentinfo',
+            children : [
+                small({
+                    title : 'Наша студия работает ежедневно с девяти утра до двенадцати ночи.',
+                    className : 'dim',
+                    innerHTML : [
+                        '&#128197;',
+                        nbsp(3),
+                        time('7').outerHTML,
+                        nbsp(2),
+                        span({ className : 'dim', children : '/' }).outerHTML,
+                        nbsp(2),
+                        time('7').outerHTML,
+                        nbsp(6),
+                        '&#128344;',
+                        nbsp(3),
+                        time(b('9')).outerHTML,
+                        nbsp(2),
+                        span({ className : 'dim', children : '—' }).outerHTML,
+                        nbsp(2),
+                        time(b('24')).outerHTML,
+                        nbsp(1),
+                        sup({ className : 'dim', children : '±' }).outerHTML,
+                    ].join('')
+                }),
+                address(small(a({
+                    href : 'mailto:vv.aristov@gmail.com',
+                    className : 'dim',
+                    innerHTML : `©${ nbsp(2) + CURRENT_YEAR + nbsp(3) }студия «Фьюжн»`
+                })))
+            ]
+        }),
+    ]
+})
+
+document.head.append(
     link({
         rel : 'stylesheet',
         href : '/htmlmodule/docs/data/studio.css'
-    })
-])
+    }),
+    script({ src : '/htmlmodule/docs/data/studio-back.js' }))
