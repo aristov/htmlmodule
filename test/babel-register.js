52c0050cecfa41a13e3b799b48e93503899c7eaa
aristov <vv.aristov@gmail.com>
2018-04-15 21:12:30 +0300

    Update dommodule version

--- test/babel-register.js
+++ test/babel-register.js
@@ -1,4 +1,4 @@
 // https://stackoverflow.com/questions/35040978/babel-unexpected-token-import-when-running-mocha-tests
 require('babel-register')({
-    ignore: /node_modules\/(?!dommodule)/
+    ignore: /node_modules\/(?!esmodule|dommodule)/
 })
