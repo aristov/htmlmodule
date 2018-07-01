7276b821724b761a8d725357ad5aac50ed5a507a
aristov <vv.aristov@gmail.com>
2018-07-01 14:34:40 +0300

    NPM: update dev dependencies, fix Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,36 @@
 'use strict'
 
 const path = require('path')
-const { optimize : { UglifyJsPlugin } } = require('webpack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const distPath = path.join(__dirname, 'dist')
 const babelLoader = {
     test : /\.js$/,
-    loader : 'babel-loader'
+    use : { loader : 'babel-loader' }
 }
 const uglifyJsPlugin = new UglifyJsPlugin({
-    compress : { warnings : false },
-    mangle : { keep_fnames : true },
-    comments : false
+    uglifyOptions : {
+        keep_fnames : true,
+        keep_classnames : true,
+        output : {
+            comments : false
+        }
+    }
 })
 
 module.exports = [
     {
+        mode : 'none',
         entry : './lib/index.js',
         output : {
             path : distPath,
             filename : 'dist.htmlmodule.js',
             libraryTarget : 'commonjs2'
         },
-        module : { loaders : [babelLoader] }
+        module : { rules : [babelLoader] }
     },
     {
+        mode : 'none',
         entry : './lib/index.js',
         output : {
             path : distPath,
@@ -32,35 +38,38 @@ module.exports = [
             library : 'htmlmodule',
             libraryTarget : 'window'
         },
-        module : { loaders : [babelLoader] },
+        module : { rules : [babelLoader] },
         plugins : [uglifyJsPlugin]
     },
     {
+        mode : 'none',
         entry : './shim/index.js',
         output : {
             path : distPath,
             filename : 'dist.shim.js'
         },
-        module : { loaders : [babelLoader] },
+        module : { rules : [babelLoader] },
         plugins : [uglifyJsPlugin]
     },
     {
+        mode : 'none',
         entry : './spec/index.spec.js',
         output : {
             path : path.join(__dirname, 'docs', 'build'),
             filename : 'build.spec.js'
         },
-        module : { loaders : [babelLoader] }
+        module : { rules : [babelLoader] }
     },
     {
+        mode : 'none',
         entry : './docs/index.js',
         output : {
             path : path.join(__dirname, 'docs', 'build'),
             filename : 'build.app.js'
         },
         module : {
-            loaders : [babelLoader],
             rules : [
+                babelLoader,
                 {
                     test : /\.css$/,
                     use : [
