02f662cf8628bc3c90fe07119a9a7ba32f3864bb
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-05-01 19:43:54 +0300

    Using morphdom

--- docs/Counter.js
+++ docs/Counter.js
@@ -1,18 +1,40 @@
-import { HtmlDiv, HtmlButton, HtmlSpan } from '../lib'
+import morphdom from 'morphdom'
+import { HtmlDiv, HtmlButton, HtmlSpan, DomFragment } from '../lib'
 
 export class Counter extends HtmlDiv
 {
-  build() {
+  constructor(init) {
+    super(init)
+    this.state = {}
+    this.setState({ counter : 0 })
+  }
+
+  render() {
     return [
       new HtmlButton({
         text : '−',
-        onclick : () => this._span.text--
+        onclick : () => this.setState({ counter : this.state.counter - 1 }),
       }),
-      this._span = new HtmlSpan(0),
+      new HtmlSpan(this.state.counter),
       new HtmlButton({
         text : '+',
-        onclick : () => this._span.text++
+        onclick : () => this.setState({ counter : this.state.counter + 1 }),
       }),
     ]
+  }
+
+  setState(state) {
+    Object.assign(this.state, state)
+    const fragment = new DomFragment(this.render())
+    morphdom(this.node, fragment.node, {
+      childrenOnly : true,
+      onBeforeNodeAdded : (toNode) => console.log('onBeforeNodeAdded', toNode),
+      onNodeAdded : (toNode) => console.log('onNodeAdded', toNode),
+      onBeforeElUpdated : (fromEl, toEl) => console.log('onBeforeElUpdated', fromEl, toEl),
+      onElUpdated : (fromEl) => console.log('onElUpdated', fromEl),
+      onBeforeNodeDiscarded : (fromNode) => console.log('onBeforeNodeDiscarded', fromNode),
+      onNodeDiscarded : (fromNode) => console.log('onNodeDiscarded', fromNode),
+      onBeforeElChildrenUpdated : (fromEl, toEl) => console.log('onBeforeElChildrenUpdated', fromEl, toEl),
+    })
   }
 }
