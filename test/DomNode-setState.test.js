91859848eb94fcd8d3be6f55e0bbe4b3b8536647
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-06-13 15:04:25 +0300

    DomNode: keeping initial child instances on render

---
+++ test/DomNode-setState.test.js
@@ -0,0 +1,45 @@
+const test = require('ava')
+const { HtmlArticle, HtmlButton, HtmlSection } = require('..')
+
+class Article extends HtmlArticle
+{
+  state = {
+    expanded : false,
+  }
+
+  render() {
+    return [
+      this.refs.button = new HtmlButton({
+        text : this.state.expanded ? 'Off' : 'On',
+        onclick : this.onClick,
+      }),
+      new HtmlSection({
+        hidden : !this.state.expanded,
+        children : 'Foo',
+      }),
+      this.state.expanded ? 'Bat' : 'Bar',
+    ]
+  }
+
+  onClick = () => this.setState(state => ({ expanded : !state.expanded }))
+
+  toggle() {
+    this.refs.button.click()
+  }
+}
+
+test('setState', t => {
+  const instance = Article.render()
+
+  t.is(instance.toString(), '<article class="Article"><button>On</button><section hidden="">Foo</section>Bar</article>')
+
+  instance.toggle()
+
+  t.is(instance.toString(), '<article class="Article"><button>Off</button><section>Foo</section>Bat</article>')
+
+  instance.toggle()
+
+  t.is(instance.toString(), '<article class="Article"><button>On</button><section hidden="">Foo</section>Bar</article>')
+
+  instance.destroy()
+})
--- test/DomNode.test.js
+++ test/DomNode.test.js
@@ -1,39 +1,6 @@
 const test = require('ava')
 const { EventTarget } = require('xwindow')
-const {
-  DomNode,
-  HtmlA,
-  HtmlDiv,
-  HtmlSpan,
-  HtmlArticle,
-  HtmlButton,
-  HtmlSection,
-} = require('..')
-
-class Article extends HtmlArticle
-{
-  state = { expanded : false }
-
-  render() {
-    return [
-      this._button = new HtmlButton({
-        text : this.state.expanded ? 'Off' : 'On',
-        onclick : this.onClick,
-      }),
-      new HtmlSection({
-        hidden : !this.state.expanded,
-        children : 'Lorem ipsum',
-      }),
-      this.state.expanded ? 'Click button to close' : 'Click button to open',
-    ]
-  }
-
-  onClick = () => this.setState({ expanded : !this.state.expanded })
-
-  toggle() {
-    this._button.click()
-  }
-}
+const { DomNode, HtmlA, HtmlDiv, HtmlSpan } = require('..')
 
 let instance
 
@@ -53,20 +20,6 @@ test('children', t => {
   t.is(instance.node.childNodes.length, 4)
   t.is(instance.node.childElementCount, 2)
   t.is(instance.toString(), '<div>foo<span>bar</span>bat<a>baz</a></div>')
-})
-
-test('setState', t => {
-  instance = Article.render()
-
-  t.is(instance.toString(), '<article class="Article"><button>On</button><section hidden="">Lorem ipsum</section>Click button to open</article>')
-
-  instance.toggle()
-
-  t.is(instance.toString(), '<article class="Article"><button>Off</button><section>Lorem ipsum</section>Click button to close</article>')
-
-  instance.setState(state => ({ expanded : !state.expanded }))
-
-  t.is(instance.toString(), '<article class="Article"><button>On</button><section hidden="">Lorem ipsum</section>Click button to open</article>')
 })
 
 test('destroy', t => {
