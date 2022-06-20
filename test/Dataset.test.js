a9028e5b21764dd2cc43d3e9f2f421c090b7b4dc
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-06-20 07:39:43 +0300

    Dataset: removing attribute on null value

--- test/Dataset.test.js
+++ test/Dataset.test.js
@@ -2,10 +2,11 @@ const test = require('ava')
 const { HtmlDiv } = require('..')
 
 test('test #1', t => {
-  const instance = HtmlDiv.render({ dataset : { foo : 'bar', bat : undefined } })
+  const instance = HtmlDiv.render({ dataset : { foo : 'bar', bat : null } })
 
   t.is(instance.dataset.foo, 'bar')
   t.is(instance.dataset.bat, undefined)
+  t.deepEqual(instance.dataset, { foo : 'bar' })
   t.is(instance.getAttr('data-foo'), 'bar')
   t.is(instance.getAttr('data-bat'), null)
   t.false(instance.node.hasAttribute('data-bat'))
