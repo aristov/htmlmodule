f0832e7a417e0e299571a97d883725b1d6ac57ac
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-06-20 15:31:35 +0300

    Making tagName mandatory

--- test/tagName.test.js
+++ test/tagName.test.js
@@ -1,4 +1,5 @@
 const test = require('ava')
+const { HTMLMetaElement, HTMLLinkElement } = require('xwindow')
 const { ElemType } = require('..')
 
 let instance
@@ -8,18 +9,20 @@ test.afterEach(() => instance.destroy())
 test('test #1', t => {
   class Bar extends ElemType
   {
-    static tagName = 'meta'
+    static tagName = 'META'
   }
 
   instance = Bar.render()
 
-  t.is(instance.node.tagName, 'meta')
+  t.is(instance.node.tagName, 'META')
+  t.is(instance.node.constructor, HTMLMetaElement)
   t.is(instance.toString(), '<meta>')
 })
 
 test('test #2', t => {
-  instance = ElemType.render({ tagName : 'link' })
+  instance = ElemType.render({ tagName : 'LINK' })
 
-  t.is(instance.node.tagName, 'link')
+  t.is(instance.node.tagName, 'LINK')
+  t.is(instance.node.constructor, HTMLLinkElement)
   t.is(instance.toString(), '<link>')
 })
