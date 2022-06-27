e4b9abd414ea2f1920a5a181119dfa390e5bbac2
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-06-27 10:58:36 +0300

    ElemType.attributes handling

--- test/attributes.test.js
+++ test/attributes.test.js
@@ -1,25 +1,70 @@
 const test = require('ava')
 const { ElemType } = require('..')
 
+class Input extends ElemType
+{
+  static tagName = 'INPUT'
+}
+
+class Test extends ElemType
+{
+  state = {
+    step : 0,
+  }
+
+  render() {
+    switch(this.state.step) {
+      case 0:
+        this.attributes = {
+          'aria-hidden' : 'true',
+          'aria-label' : null,
+        }
+        return new Input({
+          attributes : {
+            'aria-disabled' : 'true',
+            'aria-placeholder' : null,
+          },
+        })
+      case 1:
+        this.attributes = {
+          'aria-hidden' : 'false',
+          'aria-label' : 'Go',
+        }
+        return new Input({
+          attributes : {
+            'aria-disabled' : 'false',
+            'aria-placeholder' : 'Wow',
+          },
+        })
+      case 2:
+        this.attributes = {
+          'aria-label' : 'Think',
+        }
+        return new Input({
+          attributes : {
+            'aria-placeholder' : 'Hey'
+          }
+        })
+    }
+  }
+}
+
 test('test #1', t => {
-  const elem = ElemType.render({
-    attributes : {
-      'aria-hidden' : 'true',
-      'aria-label' : null,
-    },
-  })
+  const elem = Test.render()
 
   t.is(elem.attributes['aria-hidden'].value, 'true')
   t.is(elem.attributes['aria-label'], undefined)
-  t.is(elem.toString(), '<div aria-hidden="true"></div>')
+  t.is(elem.toString(), '<div class="Test" aria-hidden="true"><input class="Input" aria-disabled="true"></div>')
 
-  elem.setAttr('aria-hidden', 'false')
+  elem.setState({ step : 1 })
 
   t.is(elem.attributes['aria-hidden'].value, 'false')
-  t.is(elem.toString(), '<div aria-hidden="false"></div>')
+  t.is(elem.attributes['aria-label'].value, 'Go')
+  t.is(elem.toString(), '<div class="Test" aria-hidden="false" aria-label="Go"><input class="Input" aria-disabled="false" aria-placeholder="Wow"></div>')
 
-  elem.setAttr('aria-hidden', null)
+  elem.setState({ step : 2 })
 
   t.is(elem.attributes['aria-hidden'], undefined)
-  t.is(elem.toString(), '<div></div>')
+  t.is(elem.attributes['aria-label'].value, 'Think')
+  t.is(elem.toString(), '<div class="Test" aria-label="Think"><input class="Input" aria-placeholder="Hey"></div>')
 })
