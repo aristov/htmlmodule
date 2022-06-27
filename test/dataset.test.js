62345054efc94bb0a5dca2792df154bb84c7845c
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-06-27 11:23:34 +0300

    ElemType.dataset handling

--- test/dataset.test.js
+++ test/dataset.test.js
@@ -1,13 +1,64 @@
 const test = require('ava')
 const { ElemType } = require('..')
 
-test('test #1', t => {
-  const elem = ElemType.render({ dataset : { foo : 'bar', bat : null } })
+class Input extends ElemType
+{
+  static tagName = 'INPUT'
+}
 
-  t.is(elem.dataset.foo, 'bar')
-  t.is(elem.dataset.bat, undefined)
-  t.is(elem.getAttr('data-foo'), 'bar')
-  t.is(elem.getAttr('data-bat'), null)
-  t.false(elem.node.hasAttribute('data-bat'))
-  t.is(elem.toString(), '<div data-foo="bar"></div>')
+class Test extends ElemType
+{
+  state = {
+    step : 0,
+  }
+
+  render() {
+    switch(this.state.step) {
+      case 0:
+        this.dataset = {
+          foo : 'bar',
+          bat : null,
+        }
+        return new Input({
+          dataset : {
+            one : 'two',
+            three : null,
+          },
+        })
+      case 1:
+        this.dataset = {
+          foo : 'baz',
+          bat : 'qwe',
+        }
+        return new Input({
+          dataset : {
+            one : 'four',
+            three : 'five',
+          },
+        })
+      case 2:
+        this.dataset = {
+          bat : 'qaz',
+        }
+        return new Input({
+          dataset : {
+            three : 'six',
+          },
+        })
+    }
+  }
+}
+
+test('test #1', t => {
+  const elem = Test.render()
+
+  t.is(elem.toString(), '<div class="Test" data-foo="bar"><input class="Input" data-one="two"></div>')
+
+  elem.setState({ step : 1 })
+
+  t.is(elem.toString(), '<div class="Test" data-foo="baz" data-bat="qwe"><input class="Input" data-one="four" data-three="five"></div>')
+
+  elem.setState({ step : 2 })
+
+  t.is(elem.toString(), '<div class="Test" data-bat="qaz"><input class="Input" data-three="six"></div>')
 })
