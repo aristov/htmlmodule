009104de88bfc5c8c04b3df9b1239d5c640ebc85
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-08-04 11:00:48 +0300

    Bug fixing and refactoring + adding tests

--- test/HtmlType.test.js
+++ test/HtmlType.test.js
@@ -1,17 +1,30 @@
 const test = require('ava')
 const { HTMLDivElement } = require('xwindow')
-const { HtmlType } = require('..')
+const { HtmlType, HtmlInput } = require('..')
 
 test('test #1', t => {
   const elem = HtmlType.render({
     title : 'Hello world!',
     hidden : true,
+    tabIndex : -1,
     children : 'Example',
   })
 
+  t.is(elem.toString(), '<div title="Hello world!" hidden="" tabindex="-1">Example</div>')
   t.is(elem.node.constructor, HTMLDivElement)
   t.is(elem.node.title, 'Hello world!')
   t.is(elem.node.hidden, true)
+  t.is(elem.node.tabIndex, -1)
   t.is(elem.node.textContent, 'Example')
-  t.is(elem.toString(), '<div title="Hello world!" hidden="">Example</div>')
+})
+
+test('test #2', t => {
+  const elem = HtmlInput.render({
+    type : 'number',
+    valueAsNumber : 42,
+  })
+
+  t.is(elem.toString(), '<input type="number">')
+  t.is(elem.node.type, 'number')
+  t.is(elem.node.valueAsNumber, 42)
 })
