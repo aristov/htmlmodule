b98953877feb83ed293e3487b86fcfa2d380df54
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-08-20 05:45:03 +0300

    Correct handling of an undefined children result of a call to the render() method

--- test/mount.test.js
+++ test/mount.test.js
@@ -2,39 +2,76 @@ const test = require('ava')
 const sinon = require('sinon')
 const { ElemType } = require('..')
 
-const childMount = sinon.spy()
+test('test #1', t => {
+  const childMount = sinon.spy()
 
-class Child extends ElemType
-{
-  mount() {
-    childMount.apply(this, arguments)
-  }
-}
-
-class Parent extends ElemType
-{
-  state = {
-    step : 0,
-  }
-
-  render() {
-    switch(this.state.step) {
-      case 0:
-        return []
-      case 1:
-        return new Child
+  class Child extends ElemType
+  {
+    mount() {
+      childMount.apply(this, arguments)
     }
   }
-}
 
-test('test #1', t => {
+  class Parent extends ElemType
+  {
+    state = {
+      step : 0,
+    }
+
+    render() {
+      switch(this.state.step) {
+        case 0:
+          return []
+        case 1:
+          return new Child
+      }
+    }
+  }
+
   const parent = Parent.render()
 
-  t.is(childMount.callCount, 0)
   t.is(parent.toString(), '<div class="Parent"></div>')
+  t.is(childMount.callCount, 0)
 
   parent.setState({ step : 1 })
 
-  t.is(childMount.callCount, 1)
   t.is(parent.toString(), '<div class="Parent"><div class="Child"></div></div>')
+  t.is(childMount.callCount, 1)
+})
+
+test('test #2', t => {
+  const childMount = sinon.spy()
+
+  class Child extends ElemType
+  {
+    mount() {
+      childMount.apply(this, arguments)
+    }
+  }
+
+  class Parent extends ElemType
+  {
+    state = {
+      step : 0,
+    }
+
+    render() {
+      switch(this.state.step) {
+        case 0:
+          return undefined
+        case 1:
+          return new Child
+      }
+    }
+  }
+
+  const parent = Parent.render()
+
+  t.is(parent.toString(), '<div class="Parent"></div>')
+  t.is(childMount.callCount, 0)
+
+  parent.setState({ step : 1 })
+
+  t.is(parent.toString(), '<div class="Parent"><div class="Child"></div></div>')
+  t.is(childMount.callCount, 1)
 })
