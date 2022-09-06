89720e242a2ac44b73b2579e87e8c5af68f46044
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-09-06 15:30:00 +0300

    Handling the classList prop: - getting rid of the object type value; - handling the string type value as a singe item array.

--- test/classList-1.test.js
+++ test/classList-1.test.js
@@ -14,19 +14,33 @@ class Test extends ElemType
 
   state = {
     step : 0,
+    mode : 'on',
   }
 
   render() {
+    const mode = this.state.mode
     switch(this.state.step) {
       case 0:
         this.classList = ['foo', null, 'bar']
-        return new Input({ classList : ['one', null, 'two'] })
+        return new Input({
+          classList : ['one', null, 'two'],
+        })
       case 1:
-        this.classList = { foo : false, bat : true }
-        return new Input({ classList : { one : false, three : true } })
+        this.classList = [
+          mode === 'off' && 'foo',
+          mode === 'on' && 'bat',
+        ]
+        return new Input({
+          classList : [
+            mode === 'off' && 'one',
+            mode === 'on' && 'three',
+          ],
+        })
       case 2:
         this.classList = 'foo baz'
-        return new Input({ classList : 'one four' })
+        return new Input({
+          classList : 'one four',
+        })
       case 3:
         this.classList = null
         return new Input
@@ -43,9 +57,13 @@ test('test #1', t => {
 
   t.is(elem.toString(), '<div class="Test bat"><input class="Input three"></div>')
 
+  elem.setState({ mode : 'off' })
+
+  t.is(elem.toString(), '<div class="Test foo"><input class="Input one"></div>')
+
   elem.setState({ step : 2 })
 
-  t.is(elem.toString(), '<div class="foo baz"><input class="one four"></div>')
+  t.is(elem.toString(), '<div class="Test foo baz"><input class="Input one four"></div>')
 
   elem.setState({ step : 3 })
 
