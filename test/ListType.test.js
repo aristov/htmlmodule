9270e1c89e1e081f4c90407f96f4c1d46e4cc22f
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-09-08 19:55:07 +0300

    ListType implementation

---
+++ test/ListType.test.js
@@ -0,0 +1,66 @@
+const test = require('ava')
+const { ElemType, ListType } = require('..')
+
+class App extends ElemType
+{
+  static class = 'App'
+
+  static props = {
+    relevant : ListType.define('aria-relevant', ['additions', 'text']),
+    controls : ListType.define('aria-controls'),
+  }
+
+  state = {
+    relevant : null,
+    controls : null,
+  }
+
+  render() {
+    this.relevant = this.state.relevant
+    this.controls = this.state.controls
+  }
+}
+
+test('token list', t => {
+  const app = App.render()
+
+  t.is(app.toString(), '<div class="App"></div>')
+
+  app.setState({ relevant : ['additions', 'removals'] })
+
+  t.is(app.toString(), '<div class="App" aria-relevant="additions removals"></div>')
+
+  app.setState({ relevant : [] })
+
+  t.is(app.toString(), '<div class="App"></div>')
+
+  app.setState({ relevant : 'all' })
+
+  t.is(app.toString(), '<div class="App" aria-relevant="all"></div>')
+
+  app.setState({ relevant : ['additions', 'text'] })
+
+  t.is(app.toString(), '<div class="App"></div>')
+})
+
+test('idref list', t => {
+  const app = App.render()
+
+  t.is(app.toString(), '<div class="App"></div>')
+
+  app.setState({ controls : ['id1', 'id2', 'id3'] })
+
+  t.is(app.toString(), '<div class="App" aria-controls="id1 id2 id3"></div>')
+
+  app.setState({ controls : [] })
+
+  t.is(app.toString(), '<div class="App"></div>')
+
+  app.setState({ controls : 'id4' })
+
+  t.is(app.toString(), '<div class="App" aria-controls="id4"></div>')
+
+  app.setState({ controls : null })
+
+  t.is(app.toString(), '<div class="App"></div>')
+})
