4e1408be40fcebe1a050a64d279be8528c9286e7
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-09-09 21:44:10 +0300

    ElemType.closest method implementation

---
+++ test/closest.test.js
@@ -0,0 +1,25 @@
+const test = require('ava')
+const { ElemType, HtmlA, HtmlB, HtmlDiv } = require('..')
+
+test('test #1', t => {
+  let div1, div2, a, b
+  const elem = ElemType.render({
+    id : 'root',
+    children : div1 = new HtmlDiv({
+      id : 'id1',
+      children : a = new HtmlA(
+        div2 = new HtmlDiv(
+          b = new HtmlB('wow'),
+        ),
+      ),
+    }),
+  })
+
+  t.is(elem.toString(), '<div id="root"><div id="id1"><a><div><b>wow</b></div></a></div></div>')
+  t.is(b.closest(HtmlB), b)
+  t.is(b.closest(HtmlDiv), div2)
+  t.is(b.closest(HtmlA), a)
+  t.is(b.closest(ElemType), b)
+  t.is(b.closest(HtmlDiv, elem => elem.id === 'id1'), div1)
+  t.is(b.closest(ElemType, elem => elem.id === 'root'), elem)
+})
