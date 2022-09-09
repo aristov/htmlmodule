c2e5a7cdfcf6610cccd829ef44cacc7b1feb7000
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-09-09 11:53:04 +0300

    Editing module exports

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ exports = module.exports = {
   ...require('./lib/RoleType-lib'),
 }
 
+exports.AriaType = require('./lib/AriaType')
+exports.Attributes = require('./lib/Attributes')
 exports.AttrType = require('./lib/AttrType')
 exports.BooleanType = require('./lib/BooleanType')
+exports.Class = require('./lib/Class')
 exports.Context = require('./lib/Context')
+exports.Dataset = require('./lib/Dataset')
 exports.ElemType = require('./lib/ElemType')
+exports.EventType = require('./lib/EventType')
 exports.HtmlType = require('./lib/HtmlType')
 exports.Id = require('./lib/Id')
 exports.ListType = require('./lib/ListType')
 exports.PropType = require('./lib/PropType')
 exports.RoleType = require('./lib/RoleType')
+exports.Style = require('./lib/Style')
 exports.TextType = require('./lib/TextType')
 exports.TokenType = require('./lib/TokenType')
 
