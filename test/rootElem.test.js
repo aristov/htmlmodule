e8727cf600cce263045a728243c4e3ecbb3014aa
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-10-11 01:00:36 +0300

    ElemType.rootElem getter implementation

---
+++ test/rootElem.test.js
@@ -0,0 +1,19 @@
+const test = require('ava')
+const { ElemType } = require('..')
+
+test('test #1', t => {
+  let child1, child2, child3
+  const app = ElemType.render([
+    child1 = new ElemType(
+      child2 = new ElemType(
+        child3 = new ElemType('42')
+      )
+    )
+  ])
+
+  t.is(app.toString(), '<div><div><div><div>42</div></div></div></div>')
+  t.is(child1.rootElem, app)
+  t.is(child2.rootElem, app)
+  t.is(child3.rootElem, app)
+  t.is(app.rootElem, app)
+})
