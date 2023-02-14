93684bbc552a45d3fae529e5223a5dd1805f3fe0
Vyacheslav Aristov <vv.aristov@gmail.com>
2023-02-15 07:05:07 +0800

    PropType.get: return the actual value of the node property

--- test/attributes.test.js
+++ test/attributes.test.js
@@ -69,30 +69,18 @@ test('test #1', t => {
   const elem = App.render()
 
   t.is(elem.toString(), '<div role="form" class="App" aria-hidden="true"><input class="Input" aria-disabled="true"></div>')
-  t.deepEqual(elem.attributes, {
-    'aria-hidden' : 'true',
-    'aria-label' : null,
-  })
 
   elem.setState({ step : 1 })
 
   t.is(elem.toString(), '<div role="form" class="App" aria-hidden="false" aria-label="Go"><input class="Input" aria-disabled="false" aria-placeholder="Wow"></div>')
-  t.deepEqual(elem.attributes, {
-    'aria-hidden' : 'false',
-    'aria-label' : 'Go',
-  })
 
   elem.setState({ step : 2 })
 
   t.is(elem.toString(), '<div role="form" class="App" aria-label="Think"><input class="Input" aria-placeholder="Hey"></div>')
-  t.deepEqual(elem.attributes, {
-    'aria-label' : 'Think',
-  })
 
   elem.setState({ step : 3 })
 
   t.is(elem.toString(), '<div role="form" class="App" id="id1" data-id="123" style="display: inline;"><input class="Input" id="id2" data-id="456" style="display: block;"></div>')
-  t.is(elem.attributes, null)
 })
 
 test('test #2', t => {
@@ -103,9 +91,4 @@ test('test #2', t => {
   })
 
   t.is(elem.toString(), '<div role="form" class="App" aria-busy="true" aria-hidden="true"><input class="Input" aria-disabled="true"></div>')
-  t.deepEqual(elem.attributes, {
-    'aria-busy' : 'true',
-    'aria-hidden' : 'true',
-    'aria-label' : null,
-  })
 })
