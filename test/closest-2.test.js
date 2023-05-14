edc02b25e7a7d844b40ddd043219c0301f1c6024
Vyacheslav Aristov <vv.aristov@gmail.com>
2023-05-14 13:18:14 +0800

    ElemType: correct handling of this.__parent (bug fix)

---
+++ test/closest-2.test.js
@@ -0,0 +1,57 @@
+const test = require('ava')
+const { ElemType, HtmlA, HtmlB } = require('..')
+
+class A extends HtmlA
+{
+  static class = 'A'
+}
+
+class B extends HtmlB
+{
+  static class = 'B'
+}
+
+class App extends ElemType
+{
+  state = {
+    step : 0,
+  }
+
+  render() {
+    switch(this.state.step) {
+      case 0:
+        return new HtmlA(new HtmlB(this.props.children))
+      case 1:
+        return new HtmlB(new HtmlA(this.props.children))
+      case 2:
+        return new B(new A(this.props.children))
+    }
+  }
+}
+
+test('test #2', t => {
+  const child = new ElemType('test')
+  const app = App.render(child)
+
+  t.is(app.toString(), '<div><a><b><div>test</div></b></a></div>')
+  t.is(child.closest(HtmlA), app.children[0])
+  t.is(child.closest(HtmlB), app.children[0].children[0])
+  t.is(child.closest(B), null)
+  t.is(child.closest(App), app)
+
+  app.setState({ step : 1 })
+
+  t.is(app.toString(), '<div><b><a><div>test</div></a></b></div>')
+  t.is(child.closest(HtmlA), app.children[0].children[0])
+  t.is(child.closest(HtmlB), app.children[0])
+  t.is(child.closest(B), null)
+  t.is(child.closest(App), app)
+
+  app.setState({ step : 2 })
+
+  t.is(app.toString(), '<div><b class="B"><a class="A"><div>test</div></a></b></div>')
+  t.is(child.closest(HtmlA), app.children[0].children[0])
+  t.is(child.closest(HtmlB), app.children[0])
+  t.is(child.closest(B), app.children[0])
+  t.is(child.closest(App), app)
+})
